48acf218672a3f1872d7e02d726a67a6fcc34934
Mateusz Bielak <mateusz.bielak@skygate.io>
2019-03-26 21:52:29 +0100

    Fetch and display all pokemons

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,15 +4,24 @@ export class Dashboard extends Component {
     componentDidMount() {
         fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
             .then(res => res.json())
-            .then(({ results }) => this.setState({ results }));
+            .then(({ results }) => {
+                Promise.all(results.map(({ url }) => fetch(url).then(res => res.json()))).then(
+                    pokemons => this.setState({ pokemons }),
+                );
+            });
     }
 
     render() {
         return this.state ? (
             <>
-                {this.state.results.map(({ name }, index) => (
-                    <p key={name}>
-                        {index + 1}. {name}
+                {this.state.pokemons.map((pokemon, index) => (
+                    <p key={pokemon.name}>
+                        {index + 1}. {pokemon.name}
+                        <img
+                            style={{ height: '100px', width: '100px' }}
+                            src={`https://pokeres.bastionbot.org/images/pokemon/${index + 1}.png`}
+                            alt="pokemon"
+                        />
                     </p>
                 ))}
             </>
