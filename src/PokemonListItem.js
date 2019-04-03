1abd964ecf514bf8a70bc2d6adeb1bdedf53429e
Mateusz Bielak <mateusz.bielak@skygate.io>
2019-04-03 09:16:38 +0200

    Use emotion.js and remove inline styles

--- src/PokemonListItem.js
+++ src/PokemonListItem.js
@@ -1,47 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
 import pokeball from './assets/pokeball.svg';
 import pokeballGray from './assets/pokeball-gray.svg';
 
-export const PokemonListItem = props => (
-    <div
-        style={{
-            backgroundColor: props.caught ? '#ED5564' : '#7A777E',
-            color: '#F5F7FA',
-            marginLeft: '20px',
-            position: 'relative',
-            height: '55px',
-            margin: '40px 0',
-            fontWeight: 600,
-            fontSize: '28px',
-            border: '3px solid #434A54',
-            borderTopRightRadius: '30px',
-            borderBottomRightRadius: '30px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textTransform: 'capitalize',
-        }}
-    >
-        <img
-            src={props.caught ? pokeball : pokeballGray}
-            style={{ height: '80px', position: 'absolute', top: '-3px', left: '-40px' }}
-            className="slideRight"
-            alt="what"
-        />
-        <span>{props.pokemon.name}</span>
-        <span
-            style={{
-                fontSize: '36px',
-                position: 'absolute',
-                right: '25px',
-                fontStyle: 'italic',
-            }}
-        >
-            #{props.pokemon.id}
-        </span>
-    </div>
+const Wrapper = styled.div`
+    background-color: ${props => (props.caught ? '#ED5564' : '#7A777E')};
+    color: #f5f7fa;
+    margin-left: 20px;
+    position: relative;
+    height: 55px;
+    margin: 40px 0;
+    font-weight: 600;
+    font-size: 28px;
+    border: 3px solid #434a54;
+    border-top-right-radius: 30px;
+    border-bottom-right-radius: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: capitalize;
+`;
+
+const PokeballIcon = styled.img`
+    height: 80px;
+    position: absolute;
+    top: -3px;
+    left: -40px;
+`;
+
+const PokemonHash = styled.span`
+    font-size: 36px;
+    position: absolute;
+    right: 25px;
+    font-style: italic;
+`;
+
+export const PokemonListItem = ({ caught, pokemon }) => (
+    <Wrapper caught={caught}>
+        <PokeballIcon src={caught ? pokeball : pokeballGray} className="slideRight" alt="what" />
+        <span>{pokemon.name}</span>
+        <PokemonHash>#{pokemon.id}</PokemonHash>
+    </Wrapper>
 );
 PokemonListItem.propTypes = {
     caught: PropTypes.bool.isRequired,
