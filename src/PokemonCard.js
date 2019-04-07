34b6f96313abddb2ea22b4e991e80f818717ee8a
Mateusz Bielak <mateusz.bielak@skygate.io>
2019-04-07 23:22:04 +0200

    Set blur when fetching images

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
 
-import { colors } from './variables';
+import { api, colors } from './variables';
 
 const Wrapper = styled.div`
     width: 60%;
@@ -40,12 +40,15 @@ const Image = styled.img`
     width: 100%;
     max-height: 200px;
     max-width: 200px;
+
+    filter: ${props => (props.blur ? 'blur(5px)' : 'none')};
 `;
 
 export class PokemonCard extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
+            blur: false,
             pokemon: undefined,
             pokemonSpecies: undefined,
         };
@@ -55,37 +58,41 @@ export class PokemonCard extends React.PureComponent {
         this.fetchPokemonData();
     }
 
-    componentDidUpdate() {
-        this.fetchPokemonData();
+    componentDidUpdate(prevProps) {
+        if (prevProps.pokemonId !== this.props.pokemonId) {
+            this.fetchPokemonData();
+            this.setImageBlur();
+        }
+    }
+
+    setImageBlur() {
+        this.setState({ blur: true });
     }
 
     fetchPokemonData() {
-        const requests = Promise.all([
-            fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.pokemonId}`).then(res =>
-                res.json(),
-            ),
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.props.pokemonId}`).then(res =>
-                res.json(),
-            ),
-        ]);
+        const { pokemonId } = this.props;
+        const apiPaths = [`${api}pokemon/${pokemonId}`, `${api}pokemon-species/${pokemonId}`];
 
-        requests.then(([pokemon, pokemonSpecies]) => this.setState({ pokemon, pokemonSpecies }));
+        const data = Promise.all([apiPaths.map(apiPath => fetch(apiPath).then(res => res.json()))]);
+
+        data.then(([pokemon, pokemonSpecies]) => this.setState({ pokemon, pokemonSpecies }));
     }
 
     render() {
+        const { blur, pokemon, pokemonSpecies } = this.state;
+        const { pokemonId } = this.props;
+
         return (
             <Wrapper>
                 <Header>
                     <NameCard>
-                        <Name>{this.state.pokemon && this.state.pokemon.name}</Name>
-                        <span>
-                            {this.state.pokemonSpecies && this.state.pokemonSpecies.genera[2].genus}
-                        </span>
+                        <Name>{pokemon && pokemon.name}</Name>
+                        <span>{pokemonSpecies && pokemonSpecies.genera[2].genus}</span>
                     </NameCard>
                     <Image
-                        src={`https://pokeres.bastionbot.org/images/pokemon/${
-                            this.props.pokemonId
-                        }.png`}
+                        src={`https://pokeres.bastionbot.org/images/pokemon/${pokemonId}.png`}
+                        onLoad={() => this.setState({ blur: false })}
+                        blur={blur}
                         alt="poke"
                     />
                 </Header>
